03b33cd9288827ac1352b683b686520cc5c2d905
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-09 16:00:53 +0100

    pointmap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
+import vinted from "/Users/vincentdesmont/LeReacteur/React/jour8/exercice/vinted/src/img/Vinted_logo.png";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div>
-        <img
-          className="header-logo"
-          src="public/assets/img/Vinted_logo.png"
-          alt="vinted"
-        />
+        <img className="header-logo" src={vinted} alt="vinted" />
       </div>
-
-      <Link to="/details">Vers details</Link>
+      <div className="search-container">
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Recherche des articles"
+        />
+        <div>
+          <button className="header-button button-login-signup button-signup">
+            S'inscrire
+          </button>
+          <button className="header-button button-login-signup">
+            Se connecter
+          </button>
+        </div>
+        <button className="header-button button-sold">
+          Vends tes articles
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 const Home = () => {
   const id = 1234567890;
-  return (
-    <div>
-      <p>Je suis sur la page Home</p>
+  const [counter, setCounter] = useState(0);
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://site--test-backend--c7br8w6v87r6.code.run/offers"
+      );
 
-      <Link to="/details">Aller vers la page details</Link>
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
-      <Link to={`/offer/${id}`}>Aller sur Offer</Link>
-    </div>
+  return isLoading ? (
+    <p>Loading ...</p>
+  ) : (
+    <>
+      <div className="home-hero-bg-img ">
+        {/* <Link to="/details">Aller vers la page details</Link>
+
+      <Link to={`/offer/${id}`}>Aller sur Offer</Link> */}
+      </div>
+
+      <div className="home-card-wrapper"></div>
+    </>
   );
 };
 
