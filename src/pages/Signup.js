f678e668f717ef4beca03cd18d56f4d68b458da2
vincent desmouceaux <vincentdesmont@mbp-de-vincent.lan>
2022-11-11 14:21:22 +0100

    erreur

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,31 +1,54 @@
-import { useParams } from "react-router-dom";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-const Signup = () => {
+const Signup = ({ handleToken }) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
+  const [newsletter, setNewsletter] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+
+      setErrorMessage("");
+
+      const response = await axios.post(
+        "https://site--test-backend--c7br8w6v87r6.code.run/user/signup",
+        {
+          email: email,
+          username: username,
+          password: password,
+          newsletter: newsletter,
+        }
+      );
+      console.log(response.data);
+
+      if (response.data.token) {
+        handleToken(response.data.token);
+
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+
+      if (error.response?.status === 400) {
+        setErrorMessage("Veuillez utiliser un email non déjà utilisé :)");
+      }
+
+      if (error.response?.data.message === "Missing parameters") {
+        setErrorMessage("Veuillez remplir tous les champs");
+      }
+    }
+  };
 
   return (
     <div className="signup-container">
-      <form
-        className="signup-form"
-        onSubmit={async (event) => {
-          event.preventDefault();
-          await axios.post(
-            `https://site--test-backend--c7br8w6v87r6.code.run/user/signup`,
-            {
-              username: username,
-              email: email,
-              password: password,
-            }
-          );
-          navigate("/");
-        }}
-      >
+      <form className="signup-form" onSubmit={handleSubmit}>
         <h2>S'inscrire</h2>
         <input
           type="text"
@@ -37,6 +60,7 @@ const Signup = () => {
             setUsername(event.target.value);
           }}
         />
+        <span className="signup-login-error-message">{errorMessage}</span>
         <input
           type="text"
           name="email"
@@ -47,6 +71,7 @@ const Signup = () => {
             setEmail(event.target.value);
           }}
         />
+        <span className="signup-login-error-message">{errorMessage}</span>
         <input
           type="text"
           name="password"
@@ -61,8 +86,25 @@ const Signup = () => {
         <button className="cursor" type="submit" value="Register">
           S'inscrire
         </button>
+        <div className="checkbox-container">
+          <div>
+            <input
+              type="checkbox"
+              checked={newsletter}
+              onChange={() => {
+                setNewsletter(!newsletter);
+              }}
+            />
+            <span>S'inscrire à notre newsletter</span>
+          </div>
+          <p>
+            En m'inscrivant je confirme avoir lu et accepté les Termes &
+            Conditions et Politique de Confidentialité de Vinted. Je confirme
+            avoir au moins 18 ans.
+          </p>
+        </div>
       </form>
-      <Link to={`/user/signup`}> Tu as déjà un compte ? Connecte-toi !</Link>
+      <Link to={`/login`}> Tu as déjà un compte ? Connecte-toi !</Link>
     </div>
   );
 };
